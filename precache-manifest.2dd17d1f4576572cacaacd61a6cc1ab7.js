10025d69128776a3c5a45c98166e21a80491bc5f
Pavlikova <katpavlik@gmail.com>
2020-08-06 13:08:30 +0200

    Updates

---
+++ precache-manifest.2dd17d1f4576572cacaacd61a6cc1ab7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33843db3df97717b6378d1b0bb9a071",
+    "url": "/vzkazovnicek/index.html"
+  },
+  {
+    "revision": "ac24947b5e98dd2b192f",
+    "url": "/vzkazovnicek/static/css/main.7cefb129.chunk.css"
+  },
+  {
+    "revision": "47332daaac2e855386ba",
+    "url": "/vzkazovnicek/static/js/2.78daf6c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vzkazovnicek/static/js/2.78daf6c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac24947b5e98dd2b192f",
+    "url": "/vzkazovnicek/static/js/main.a6c4c556.chunk.js"
+  },
+  {
+    "revision": "5f3bc24c786560534030",
+    "url": "/vzkazovnicek/static/js/runtime-main.415da9b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vzkazovnicek/static/media/logo.5d5d9eef.svg"
+  }
+]);
